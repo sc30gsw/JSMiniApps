72e551a00630160b7f7144179fb9f1b6384dc2c5
sc30gsw <haidoushi875@gmail.com>
2022-04-22 06:32:13 +0900

    数値と記号をパスワードに含めるよう処理を追加

--- PasswordGenerator/js/main.js
+++ PasswordGenerator/js/main.js
@@ -5,13 +5,31 @@
   const label = document.getElementById('label');
   const btn = document.getElementById('btn');
   const result = document.getElementById('result');
+  const numbers = document.getElementById('numbers');
+  const symbols = document.getElementById('symbols');
 
   // パスワードを表示する関数
   function getPassword() {
-    const seed = 'abcdefghijklmnopqrstuvwxyz';
+    const seedLetters = 'abcdefghijklmnopqrstuvwxyz';
+    const seedNumbers = '0123456789';
+    const seedSymbols = '!#$%&?@()+*-_';
+    let seed;
     // 何文字のパスワードを生成するかの数値を格納する
     let len = slider.value;
     let pwd = '';
+
+    // seedにアルファベットの大文字・小文字を追加する
+    seed = seedLetters + seedLetters.toUpperCase();
+
+    // numbersがチェックされた場合
+    if (numbers.checked) {
+      seed += seedNumbers;
+    }
+
+    // symbolsがチェックされた場合
+    if (symbols.checked) {
+      seed += seedSymbols;
+    }
 
     // 文字数分ランダムな文字列(パスワード)を生成する
     // lenが0になるまで繰り返す
