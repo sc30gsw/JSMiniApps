e473b5622772c194cb4c8bb4da47ed0ad15593d1
sc30gsw <haidoushi875@gmail.com>
2022-04-28 18:25:44 +0900

    タイプキーが正しい場合に文字列が置き換わる処理の作成

--- MyTypingGame/js/main.js
+++ MyTypingGame/js/main.js
@@ -1,10 +1,24 @@
 'use strict';
 
 {
+  // タイプしていく単語を定義
+  const word = 'red';
+  // 現在、入力している文字が何文字目かを判定する変数
+  let loc = 0;
+
+  const target = document.getElementById('target');
+  // 要素のテキストを入力されたキーにする
+  target.textContent = word;
+
   /** キーボードで入力した際にイベント発火 */
   document.addEventListener('keydown', e => {
-    const target = document.getElementById('target');
-    // 要素のテキストを入力されたキーにする
-    target.textContent = e.key;
-  })
+    
+    // タイプしたキーが正しいか場合(1文字ずつ判定する)
+    if (e.key === word[loc]) {
+      // 次の文字の判定ができるようにする
+      loc ++;
+      // アンダーバーをlocの個数分コピーし、変数wordのloc番目の文字列と結合させる
+      target.textContent = '_'.repeat(loc) + word.substring(loc);
+    }
+  });
 }
